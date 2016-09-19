f3a3174344b51fac9bc06bd7acccf84a95d8e0be
Noura Azeem <noura.azeem@gmail.com>
2016-09-19 19:08:09 -0400

    Add files via upload
    Quiz 1 practice problems

---
+++ practiceforquiz1.py
@@ -0,0 +1,16 @@
+
+first_name =input("What is your first name ")
+last_name=input("what is your last name?")
+print("Your full name is " , first_name + last_name)
+
+x=137
+a= 0 or x
+print( a and x==a)
+
+print("Choose any 3 numbers")
+
+first_number = input("First number ")
+second_number = input("Second number")
+third_number = input("Third number")
+
+print("Your three numbers together are ", (int(first_number) + int(second_number) + int(third_number)))
